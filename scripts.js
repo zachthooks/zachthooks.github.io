441f0cfb7a942a20e623fc0c10010f3f70971aef
Zach Hooks <zhooks558@gmail.com>
2024-08-13 14:46:54 -0500

    Add portfolio files

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+
+    const options = {
+        root: null,
+        threshold: 0.1,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
